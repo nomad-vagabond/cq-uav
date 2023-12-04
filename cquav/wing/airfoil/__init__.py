a97f5d566fb8e9e51bbe8af04685af0851884931
nomad-vagabond <keenon3d@gmail.com>
2023-12-04 23:30:20 +0200

    Include airfoils_collection.json in the package build

--- cquav/wing/airfoil/__init__.py
+++ cquav/wing/airfoil/__init__.py
@@ -4,8 +4,8 @@ from .airfoil import Airfoil
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 def load_airfoils_collection():
-    collecion_path = os.path.join(current_dir, "airfoils_collection.json")
-    with open(collecion_path) as ac:
+    collection_path = os.path.join(current_dir, "airfoils_collection.json")
+    with open(collection_path) as ac:
         airfoils_data = json.loads(ac.read())
     
     return airfoils_data
--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='cquav',
-    version='0.0.1',
+    version='0.0.2',
     author = "Vadym Pasko",
     author_email = "vadym@splinecloud.com",
-    description = "A CaqQuery library for eneratingUAV components.",
+    description = "A CaqQuery library for generating UAV components.",
     license = "MIT",
-    packages=find_packages(),
+    packages=find_packages(),  
+    package_data = {"cquav": ["wing/airfoil/*.json"]},
+    include_package_data=True,
     install_requires=[
         'numpy',
         'scipy',

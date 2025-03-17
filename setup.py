bd236cc62ec904e9226363d47038142b3a40033b
nomad-vagabond <keenon3d@gmail.com>
2025-03-18 00:06:42 +0200

    Upgrade setup.py to new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cquav',
-    version='0.0.6',
+    version='0.0.7',
     author = "Vadym Pasko",
     author_email = "vadym@splinecloud.com",
     description = "A CaqQuery library for generating UAV components.",
@@ -11,11 +11,11 @@ setup(
     package_data = {"cquav": ["wing/airfoil/*.json"]},
     include_package_data=True,
     install_requires=[
+        'cadquery',
+        'splinecloud-scipy',
+        'requests',
         'numpy',
         'scipy',
         'pandas',
-        'cadquery',
-        'requests',
-        'splinecloud-scipy',
     ],
 )
